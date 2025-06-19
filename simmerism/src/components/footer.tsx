7afba4a37853ac0de70e407b90d599de9ce9e3e9
Vado-hyh <vul4ul4cjo@gmail.com>
2025-06-19 18:16:15 +0800

    調整footer並加上footer的RWD

--- simmerism/src/components/footer.tsx
+++ simmerism/src/components/footer.tsx
@@ -1,15 +1,27 @@
 export default function Footer() {
   return (
-    <footer className="bg-[#5a9a8e] py-16 text-white">
-      <div className="container mx-auto px-4">
-        <div className="text-[120px] font-bold leading-none">
-          <span className="text-white">SIMMER</span>
-          <span className="text-[#ff6347]">ISM</span>
+    <footer className="bg-[#5a9a8e] py-16 text-white overflow-hidden">
+      <div className="w-full">
+        {/* 寬度超出畫面 + 微調右偏 */}
+        <div className="w-[110vw] mx-auto translate-x-[1vw]">
+          <h1
+            className="font-extrabold uppercase leading-none whitespace-nowrap text-white"
+            style={{
+              fontSize: '17.2vw',
+              lineHeight: 1,
+              letterSpacing: '-0.04em',
+            }}
+          >
+            <span className="text-white">SIMMER</span>
+            <span className="text-[#ff6347]">ISM</span>
+          </h1>
         </div>
-        <div className="mt-4">
+
+        {/* 版權文字置中 */}
+        <div className="mt-4 text-center">
           <p className="text-sm">© 2025 Simmerism. All rights reserved.</p>
         </div>
       </div>
     </footer>
-  )
+  );
 }
